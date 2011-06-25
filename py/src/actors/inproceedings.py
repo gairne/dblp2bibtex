3a764f47de6ca1067ae7d4cd731f905b21494ac2
Matthew Mole <mrm30@kent.ac.uk>
2011-06-25 15:00:16 +0100

    Added Creative Commons CC BY-NC-SA 3.0 License

---
+++ py/src/actors/inproceedings.py
@@ -0,0 +1,7 @@
+# From http://en.wikipedia.org/wiki/Bibtex
+# inproceedings
+#
+#    An article in a conference proceedings.
+#    Required fields: author, title, booktitle, year
+#    Optional fields: editor, series, pages, organization, publisher, address, month, note, key
+
---
+++ py/src/actors/proceedings.py
@@ -0,0 +1,7 @@
+# From http://en.wikipedia.org/wiki/Bibtex
+# proceedings
+#
+#    The proceedings of a conference.
+#    Required fields: title, year
+#    Optional fields: editor, publisher, organization, address, month, note, key
+
